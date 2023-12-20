5b607a7bf059ae1ccc142d04991119ff93110d51
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-12-20 17:30:41 -0500

    wip documentation

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,17 @@
+// get counter paragraph element from dom
 const doc_visitor_count = document.querySelector("#counter p");
-const canvas = document.querySelector("canvas");
 
+// send a post request to Azure Function
 fetch("https://austinsresumefunction.azurewebsites.net/api/httpexample", {
     method: "POST",
     headers: {
         "Accept": "application/json",
     }
 }).then(
+    // Get response and parse JSON data
     res => res.json()
 ).then(data => {
+    // change html element's content to the `count` property of the response
+    // data
     doc_visitor_count.innerHTML = data.count.toString()
 })
